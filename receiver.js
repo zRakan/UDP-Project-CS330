e9053916708256e5e8528d253275fc456ca5aa15
zRakan <122341529+zRakan@users.noreply.github.com>
2023-09-30 20:49:10 +0300

    Translated received packets based on packet... header

--- receiver.js
+++ receiver.js
@@ -3,6 +3,9 @@ import * as UDP from 'dgram';
 // Import utils.js functions
 import * as utils from './utils.js';
 
+// Import createPacket
+import { createPacket } from './packet.js';
+
 // Creating UDP socket (IPv4)
 const server = UDP.createSocket('udp4'); // udp6: If you want to create IPv6
 
@@ -19,11 +22,63 @@ server.on('error', function(err) {
 
 
 // Receive message(s) from client
+
 server.on('message', function(message, rInfo) {
+    const messageHeader = message.subarray(0, 9);
+    const messageContent = message.subarray(9);
+
+    const packetType = messageHeader[0];
+    const packetSequence = messageHeader.subarray(3, 7);
+
     utils.log('Server', `
-        Message Received:
+        Packet Received:
             IP: ${rInfo.address}:${rInfo.port} [${rInfo.family}]
+            Packet Type: ${packetType == 0x01 ? 'Data Packet' : 'Handshaking Packet'} ${packetType == 0x01 && (`(Seq #: ${packetSequence.readInt16LE(0)})`)}
+
     `);
+
+    switch(packetType) { // Packet types
+        case 0x01: // Data
+            const dataType = messageHeader[8];
+            
+
+            if(dataType == 0x01) { // Metadata of file
+                const fileName_OFFSET = messageContent.indexOf('__FILENAME__');
+                const fileSize_OFFSET = messageContent.indexOf('__FILESIZE__');
+
+                const fileName = messageContent.subarray(fileName_OFFSET+12, fileSize_OFFSET).toString('utf8');
+                const fileSize = messageContent.subarray(fileSize_OFFSET+12).toString('utf8');
+
+                utils.log('Server', `
+        File Metadata:
+            File name: ${fileName}
+            File size: ${utils.formatBytes(fileSize)}
+`)
+
+                // Creating file stream for writing
+                utils.startStream(fileName);
+
+            } else {
+                console.log("Writing process...");
+
+                utils.writingStream(messageContent); // Writing stream file
+
+                if(dataType == 0x02) { // Stop writing stream file
+                    utils.closeStream();
+                    utils.log('Server', 'Finished uploading ')
+                }
+            }
+
+
+            // Sending Acknoledgment packet
+            server.send(createPacket(0x02), rInfo.port, rInfo.address);
+
+            break;
+        case 0x03: // Handshake
+            server.send(Buffer.from([0x03, 0x05]), rInfo.port, rInfo.address); 
+            console.log("Test");
+            break;
+    }
 });
 
 
